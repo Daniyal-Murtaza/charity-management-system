5f66bbe3e1ceeade13d80b5009d93f864f2fa8b1
Daniyal-Murtaza <86107799+Daniyal-Murtaza@users.noreply.github.com>
2022-12-25 00:08:09 +0500

    form21-cs

---
+++ App/Form21.cs
@@ -0,0 +1,55 @@
+﻿using SPF.BLL;
+using SPF.DAL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SPF
+{
+    public partial class Form21 : Form
+    {
+        public Form21()
+        {
+            InitializeComponent();
+        }
+        volunteer_tblBLL l = new volunteer_tblBLL();
+        volunteer_tblDAL dal = new volunteer_tblDAL();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            // l.usertype = radioButton1.Text;
+
+            //Check the Login Credentials
+            l.cnic = textBox1.Text;
+
+            bool isSuccess = dal.loginCheck(l);
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("Login Failed.");
+                
+            }
+            else
+            {
+                this.Hide();
+                Form14 f14 = new Form14();
+                f14.ShowDialog();
+            }
+
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form13 f13 = new Form13();
+            f13.ShowDialog();
+        }
+    }
+}
