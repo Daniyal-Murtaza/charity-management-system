8b736eb7f9a43d9422e9db47609e3dd71d992eb4
Daniyal-Murtaza <86107799+Daniyal-Murtaza@users.noreply.github.com>
2022-12-24 23:45:06 +0500

    form12-cs

---
+++ App/Form12.cs
@@ -0,0 +1,51 @@
+﻿using SPF.DAL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SPF
+{
+    public partial class Form12 : Form
+    {
+        public Form12()
+        {
+            InitializeComponent();
+        }
+        repDAL dal = new repDAL();
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            //Let's Add the Dunctionality to Search the Donors
+
+            //1. Get the Keywords Typed on the Search TExt Box
+            string keywords = txtSearch.Text;
+
+            // Check Whether the Search TExtBox is Empty or Not
+            if (keywords != null)
+            {
+                //Display the information of Donors Based on Keywords
+                DataTable dt = dal.Search(keywords);
+                dgvDonors.DataSource = dt;
+            }
+            else
+            {
+                //DIsplay all the Donors
+                DataTable dt = dal.Select();
+                dgvDonors.DataSource = dt;
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form13 f13 = new Form13();
+            f13.ShowDialog();
+        }
+    }
+}
